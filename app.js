c258fb2297cc91a2826b9c3957c4ae435f3c69f4
MatiasRosetani <matiasrose99@gmail.com>
2023-07-15 17:48:46 -0300

    render

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv').config();
+
 const app = express();
 
 app.use('/static', express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log('Servidor funcionando');
-});
-
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/home.html');
 });
-app.get('/login', (req,res)=>{
+
+app.get('/login', (req, res) => {
     res.sendFile(__dirname + '/views/login.html');
 });
 
-app.get('/register', (req,res)=>{
+app.get('/register', (req, res) => {
     res.sendFile(__dirname + '/views/register.html');
 });
+
+const PORT = 3030;
+app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT} `))
