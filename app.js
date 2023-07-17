fa0fff703c88287c28fef12043447b32a3c1455e
MatiasRosetani <matiasrose99@gmail.com>
2023-07-17 17:43:59 -0300

    prueba

--- app.js
+++ app.js
@@ -4,18 +4,37 @@ const path = require('path');
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, './public');
-app.use('/static', express.static(__dirname + '/public'));
+const rutaPublic = path.join(__dirname, 'public');
 
-app.get('/', (req, res)=>res.sendFile(path.join(__dirname,'views/home.html'))); 
+app.use(express.static(rutaPublic));
 
-app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/views/login.html');
-});
+app.get('/', (reg, res) => {
+    const ruta = path.join(__dirname, './views/home.htmI');
+    res.sendFile(ruta);
 
-app.get('/register', (req, res) => {
-    res.sendFile(__dirname + '/views/register.html');
-});
+app - get('/login', (reg, res) => {
+     const ruta = path.join(_dirname, './views/login.htmI');
+    res.sendFile(ruta);
 
-const PORT = 3030;
-app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT} `))
+app-get('/register',(reg,res)=>{
+    const ruta = path. join(__dirname, './views/register.htmi')
+    res.sendFile (ruta);
+    });
+    
+
+        /* app.use('/static', express.static(__dirname + '/public'));
+         */
+        /* app.get('/', (req, res) => {
+            res.sendFile(__dirname + '/views/home.html');
+        });
+        
+        app.get('/login', (req, res) => {
+            res.sendFile(__dirname + '/views/login.html');
+        });
+        
+        app.get('/register', (req, res) => {
+            res.sendFile(__dirname + '/views/register.html');
+        }); */
+
+        const PORT = 3030;
+        app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT} `))
